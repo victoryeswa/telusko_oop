c12675a1d1485ed71f3abf67ecc387538a8b0207
victor yeswa <victoryeswa01@gmail.com>
2023-05-28 11:19:52 +0300

    Create oop7.py

---
+++ oop7.py
@@ -0,0 +1,28 @@
+class Student:
+    
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno = rollno
+    
+    
+    def show(self):
+        print(self.name, self.rollno)
+        self.lap.show()
+
+    class Laptop:
+
+        def __init__(self):
+            self.brand = 'HP'
+            self.cpu = 'i5'
+            self.ram = 8
+
+        def show(self):
+            print(self.brand, self.cpu, self.ram)
+
+s1 = Student('Navin', 2)
+s2 = Student('Jenny', 3)
+
+s1.show()
+
+lap1 = Student.Laptop()
+
