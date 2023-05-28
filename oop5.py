36e1926d796c6b021587ac1f24034d9705d5ba8f
victor yeswa <victoryeswa01@gmail.com>
2023-05-28 11:20:00 +0300

    Create oop5.py

---
+++ oop5.py
@@ -0,0 +1,27 @@
+class Student:
+
+    school = 'Telusko'
+
+    def __init__(self, m1, m2, m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def avg(self):
+        return (self.m1 + self.m2 + self.m3)/3
+    
+    @classmethod
+    def info(cls):
+        return cls.school
+    
+    @staticmethod
+    def info():
+        print('This is Student class.. in abc module')
+    
+s1 = Student(34, 47, 32)
+s2 = Student(89, 32, 12)
+
+print(s1.avg())
+print(Student.info())  
+
+Student.info()
