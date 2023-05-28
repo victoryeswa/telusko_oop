ae672c3130928a3e4b8913dab4dcc66980fc6646
victor yeswa <victoryeswa01@gmail.com>
2023-05-28 11:20:10 +0300

    Create oop3.py

---
+++ oop3.py
@@ -0,0 +1,15 @@
+class Car:
+
+    wheels = 4
+
+    def __init__(self):
+        self.mil = 10
+        self.com = 'BMW'
+
+c1 = Car()
+c2 = Car()
+
+c1.mil = 0
+
+print(c1.com, c1.mil, c1.wheels)
+print(c2.com, c2.mil, c2.wheels)
